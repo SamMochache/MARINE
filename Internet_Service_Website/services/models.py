7cd3ab055fff448c292855c1b07f1baa1304566a
SamMochache <sammochache01@gmail.com>
2025-02-18 17:24:31 +0300

    Creating user models and naming installed apps

--- Internet_Service_Website/services/models.py
+++ Internet_Service_Website/services/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class InternetPlan(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    speed = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class CustomerReview(models.Model):
+    customer_name = models.CharField(max_length=100)
+    review_text = models.TextField()
+    rating = models.IntegerField()
+    plan = models.ForeignKey(InternetPlan, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.customer_name} - {self.rating}/5"
